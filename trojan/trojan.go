8e15159c98bea94b48cd17903918fdeb57d5d585
Jrohy <euvkzx@gmail.com>
2020-02-26 21:07:14 +0800

    实现容器内trojan重启等操作

--- trojan/trojan.go
+++ trojan/trojan.go
@@ -21,29 +21,58 @@ func ControllMenu() {
 }
 
 func Restart() {
-	if err := util.ExecCommand("systemctl restart trojan"); err != nil {
-		fmt.Println(util.Red("重启trojan失败!"))
-	} else {
+	if util.IsExists("/.dockerenv") {
+		Stop()
+		Start()
 		fmt.Println(util.Green("重启trojan成功!"))
+	} else {
+		if err := util.ExecCommand("systemctl restart trojan"); err != nil {
+			fmt.Println(util.Red("重启trojan失败!"))
+		} else {
+			fmt.Println(util.Green("重启trojan成功!"))
+		}
 	}
 }
 
 func Start() {
-	if err := util.ExecCommand("systemctl start trojan"); err != nil {
-		fmt.Println(util.Red("启动trojan失败!"))
-	} else {
+	if util.IsExists("/.dockerenv") {
+		check := util.ExecCommandWithResult(`ps aux|grep "/usr/bin/trojan/trojan"|grep -v grep`)
+		if check != "" {
+			Stop()
+		}
+		util.ExecCommand(`echo "" > /.run.log`)
+		util.ExecCommand(`/usr/bin/trojan/trojan -c /usr/local/etc/trojan/config.json -l /.run.log &`)
 		fmt.Println(util.Green("启动trojan成功!"))
+	} else {
+		if err := util.ExecCommand("systemctl start trojan"); err != nil {
+			fmt.Println(util.Red("启动trojan失败!"))
+		} else {
+			fmt.Println(util.Green("启动trojan成功!"))
+		}
 	}
 }
 
 func Stop() {
-	if err := util.ExecCommand("systemctl stop trojan"); err != nil {
-		fmt.Println(util.Red("停止trojan失败!"))
-	} else {
+	if util.IsExists("/.dockerenv") {
+		util.ExecCommand(`ps aux|grep "/usr/bin/trojan/trojan"|grep -v grep|awk '{print $2}'|xargs -r kill -9 2>/dev/null`)
 		fmt.Println(util.Green("停止trojan成功!"))
+	} else {
+		if err := util.ExecCommand("systemctl stop trojan"); err != nil {
+			fmt.Println(util.Red("停止trojan失败!"))
+		} else {
+			fmt.Println(util.Green("停止trojan成功!"))
+		}
 	}
 }
 
 func Status() {
-	util.ExecCommand("systemctl status trojan")
+	if util.IsExists("/.dockerenv") {
+		if util.ExecCommandWithResult("cat /.run.log|grep FATAL") != "" {
+			fmt.Println(util.ExecCommandWithResult("cat /.run.log"))
+		} else {
+			fmt.Println(util.Green("trojan running..."))
+		}
+	} else {
+		util.ExecCommand("systemctl status trojan")
+	}
 }
