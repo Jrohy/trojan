cbb53241b5342fa6b675d6440cef326b7550dde9
Jrohy <euvkzx@gmail.com>
2020-06-10 10:01:46 +0800

    添加 trojan-go 安装

--- asset/trojan-install.sh
+++ asset/trojan-install.sh
@@ -2,6 +2,11 @@
 # source: https://github.com/trojan-gfw/trojan-quickstart
 set -euo pipefail
 
+# trojan: 0, trojan-go: 1
+TYPE=0
+
+[[ $1 == "go" ]] && TYPE=1
+
 function prompt() {
     while true; do
         read -p "$1 [y/N] " yn
@@ -22,11 +27,21 @@ if [[ $(uname -m 2> /dev/null) != x86_64 ]]; then
     exit 1
 fi
 
+if [[ $TYPE == 0 ]];then
+    CHECKVERSION="https://api.github.com/repos/trojan-gfw/trojan/releases/latest"
+else
+    CHECKVERSION="https://api.github.com/repos/p4gefau1t/trojan-go/releases"
+fi
 NAME=trojan
-CHECKVERSION="https://api.github.com/repos/trojan-gfw/trojan/releases/latest"
-VERSION=$(curl -H 'Cache-Control: no-cache' -s "$CHECKVERSION" | grep 'tag_name' | cut -d\" -f4 | sed 's/v//g')
-TARBALL="$NAME-$VERSION-linux-amd64.tar.xz"
-DOWNLOADURL="https://github.com/trojan-gfw/$NAME/releases/download/v$VERSION/$TARBALL"
+VERSION=$(curl -H 'Cache-Control: no-cache' -s "$CHECKVERSION" | grep 'tag_name' | cut -d\" -f4 | sed 's/v//g' | head -n 1)
+if [[ $TYPE == 0 ]];then
+    TARBALL="$NAME-$VERSION-linux-amd64.tar.xz"
+    DOWNLOADURL="https://github.com/trojan-gfw/$NAME/releases/download/v$VERSION/$TARBALL"
+else
+    TARBALL="trojan-go-linux-amd64.zip"
+    DOWNLOADURL="https://github.com/p4gefau1t/trojan-go/releases/download/v$VERSION/$TARBALL"
+fi
+
 TMPDIR="$(mktemp -d)"
 INSTALLPREFIX="/usr/bin/$NAME"
 SYSTEMDPREFIX=/etc/systemd/system
@@ -45,7 +60,21 @@ echo Downloading $NAME $VERSION...
 curl -LO --progress-bar "$DOWNLOADURL" || wget -q --show-progress "$DOWNLOADURL"
 
 echo Unpacking $NAME $VERSION...
-tar xf "$TARBALL"
+if [[ $TYPE == 0 ]];then
+    tar xf "$TARBALL"
+else
+    if [[ -z `command -v unzip` ]];then
+        if [[ `command -v dnf` ]];then
+            dnf install unzip -y
+        elif [[ `command -v yum` ]];then
+            yum install unzip -y
+        elif [[ `command -v apt-get` ]];then
+            apt-get install unzip -y
+        fi
+    fi
+    unzip "$TARBALL"
+fi
+
 cd "$NAME"
 
 echo Installing $NAME $VERSION to $BINARYPATH...
@@ -53,7 +82,11 @@ install -Dm755 "$NAME" "$BINARYPATH"
 
 echo Installing $NAME server config to $CONFIGPATH...
 if ! [[ -f "$CONFIGPATH" ]] || prompt "The server config already exists in $CONFIGPATH, overwrite?"; then
-    install -Dm644 examples/server.json-example "$CONFIGPATH"
+    if [[ $TYPE == 0 ]];then
+        install -Dm644 examples/server.json-example "$CONFIGPATH"
+    else
+        install -Dm644 example/server.json "$CONFIGPATH"
+    fi
 else
     echo Skipping installing $NAME server config...
 fi
@@ -64,7 +97,6 @@ if [[ -d "$SYSTEMDPREFIX" ]]; then
         cat > "$SYSTEMDPATH" << EOF
 [Unit]
 Description=$NAME
-Documentation=https://trojan-gfw.github.io/$NAME/config https://trojan-gfw.github.io/$NAME/
 After=network.target network-online.target nss-lookup.target mysql.service mariadb.service mysqld.service
 
 [Service]
