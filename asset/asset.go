58bf241b3e2c60b71ed243cf1a1ede7a525eee13
Jrohy <euvkzx@gmail.com>
2021-04-03 20:54:53 +0800

    修正注释

--- asset/asset.go
+++ asset/asset.go
@@ -5,7 +5,7 @@ import "embed"
 //go:embed trojan-install.sh client.json
 var f embed.FS
 
-// GetAsset 获取资源字符串
+// GetAsset 获取资源[]byte
 func GetAsset(name string) []byte {
 	data, _ := f.ReadFile(name)
 	return data
